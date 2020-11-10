1a147a9d50d5cb9bc9166ada34bcb0f55a512ee5
Bruno Henrique Souza <bruno.souza@terceiro-sky.com.br>
2020-11-10 13:37:58 -0300

    Implements tests configs

---
+++ test/index.ts
@@ -0,0 +1,48 @@
+
+import { ApiContext, ApiEvent, ApiHandler, ApiResponse, ErrorResponseBody } from '../src/shared/api.interfaces';
+import { ApiErrorResponseParsed, ApiResponseParsed, PathParameter } from './test.interfaces';
+
+type SuccessCaller = <T> (handler: ApiHandler, pathParameters?: PathParameter) => Promise<ApiResponseParsed<T>>;
+type FailureCaller = <T> (handler: ApiHandler, pathParameters?: PathParameter) => Promise<ApiErrorResponseParsed>;
+
+export const callSuccess: SuccessCaller = <T> (handler: ApiHandler, pathParameters?: PathParameter): Promise<ApiResponseParsed<T>> => {
+  return new Promise((resolve, reject) => {
+    const evt: ApiEvent = <ApiEvent> {};
+
+    if (pathParameters) {
+      evt.pathParameters = pathParameters;
+    }
+
+    handler(evt, <ApiContext> {}, (error?: Error | null | string, result?: ApiResponse): void => {
+      if (typeof result === 'undefined') {
+        reject('No result was returned by the handler!');
+        return;
+      }
+
+      const parsedResult: ApiResponseParsed<T> = result as ApiResponseParsed<T>;
+      parsedResult.parsedBody = JSON.parse(result.body) as T;
+      resolve(parsedResult);
+    });
+  });
+};
+
+export const callFailure: FailureCaller = <T> (handler: ApiHandler, pathParameters?: PathParameter): Promise<ApiErrorResponseParsed> => {
+  return new Promise((resolve, reject) => {
+    const evt: ApiEvent = <ApiEvent> {};
+
+    if (pathParameters) {
+      evt.pathParameters = pathParameters;
+    }
+
+    handler(evt, <ApiContext> {}, (error?: Error | null | string, result?: ApiResponse): void => {
+      if (typeof result === 'undefined') {
+        reject('No result was returned by the handler!');
+        return;
+      }
+
+      const parsedResult: ApiErrorResponseParsed = result as ApiErrorResponseParsed;
+      parsedResult.parsedBody = JSON.parse(result.body) as ErrorResponseBody;
+      resolve(parsedResult);
+    });
+  });
+};
